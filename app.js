5bd177d29c789784644f247105f749255a7e27ca
Dearie <eburujacintha@gmail.com>
2023-09-30 13:57:05 +0100

    Endpoint issue fixed

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const multer = require('multer');
 const shortid = require('shortid');
-const { exec } = require('child_process');
 const fs = require('fs-extra');
 
 const app = express();
@@ -20,24 +19,12 @@ app.post('/api/upload', upload.single('video'), async (req, res) => {
     // Generate a unique identifier for the video
     const videoId = shortid.generate();
 
-    // Save the uploaded video to disk (temporarily)
+    // Save the uploaded video to disk
     const videoPath = `uploads/${videoId}.webm`;
     fs.writeFileSync(videoPath, req.file.buffer);
 
-    // Compress the video using fluent-ffmpeg
-    const compressedVideoPath = `uploads/${videoId}-compressed.mp4`;
-    exec(`ffmpeg -i ${videoPath} -vcodec libx264 ${compressedVideoPath}`, (error, stdout, stderr) => {
-      if (error) {
-        console.error('Error compressing video:', error);
-        return res.status(500).json({ message: 'Error compressing video' });
-      }
-
-      // Remove the original video
-      fs.unlinkSync(videoPath);
-
-      // Return the path to the compressed video
-      res.json({ videoPath: compressedVideoPath });
-    });
+    // Return the path to the uploaded video
+    res.json({ videoPath });
   } catch (error) {
     console.error('Error uploading video:', error);
     res.status(500).json({ message: 'Error uploading video' });
@@ -47,10 +34,3 @@ app.post('/api/upload', upload.single('video'), async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-
-
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Dearie-cyber/Chrome-extension.git
-git push -u origin main
